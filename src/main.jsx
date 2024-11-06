a94a01648d4a0babcdd9c6b0eb9f50bba0dab946
COMEDIA <chronicle.co@outlook.com>
2024-11-06 14:47:13 -0600

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Logo from './assets/logo.svg'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    
+      <img src={Logo} className="logo" alt="Logo" />
+    
   </StrictMode>,
 )
